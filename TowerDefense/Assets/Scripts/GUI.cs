9046a4f0457587d04a69de6f764cacf5cf31d330
frizzlefry22 <frizzlefry22@gmail.com>
2014-12-03 00:12:23 -0800

    Updated GUI.cs
    Added the transparent PNG's
    
    Delete TDTK_Free stuff

--- TowerDefense/Assets/Scripts/GUI.cs
+++ TowerDefense/Assets/Scripts/GUI.cs
@@ -4,78 +4,152 @@ using UnityEditor;
 using UnityEngine;
 using System.Collections;
 
-public class GUI : MonoBehaviour {
+public class GUI : MonoBehaviour
+{
+    public bool TurretMenu;
 
-	// Use this for initialization
+    public bool GamePaused;
 
-    public GameObject turret1;
-    public GameObject turret2;
-
-    public Texture2D turret1texture;
-    public Texture2D turret2Texture;
-
-    public Rect Turret2Rect;
-
+    public Texture2D turret1texture2D;
+    public Texture2D turret2Texture2D;
+    public Texture2D Turret3Texture2D;
+    
     public Rect Turret1Rect;
+    public Rect Turret2Rect;
+    public Rect Turret3Rect;
 
+    private Rect _pauseMenuRect;
+
+    public Rect TurretBoarder;
+
+
+    public Rect ActiveTurret;
+    public Texture2D ActiveTurretTexture2D;
 
     void Start ()
     {
+        //Just to be safe 
+        GamePaused = false;
+        TurretMenu = false;
 
-        //foo.renderer.material.color = Color.red;
+        //Fixed in the corner can move
+        ActiveTurret = new Rect(10,Screen.height - 110 ,100,100);
+        
+        //Default Turret
+        ActiveTurretTexture2D = turret1texture2D; 
 
-        turret1texture = AssetPreview.GetAssetPreview(turret1);
-
-        turret2Texture = AssetPreview.GetAssetPreview(turret2);
-
-        //te = AssetPreview.GetMiniThumbnail(foo);
-        //this.guiTexture.texture = turret1texture;
-
-
-       // var byt = turret1texture.EncodeToPNG();
-
-        //File.WriteAllBytes(Application.dataPath + "/../SavedScreen.png", byt);
-
-        var x = 10;
-        var y =  Screen.height  - 110;
-
-
-        Turret1Rect = new Rect(x,y, 100, 100);
+        var x = (Screen.width / 2) - (400 / 2);
+        var y =  (Screen.height / 2) - (110 / 2);
+        Turret1Rect = new Rect(x,y, 100, 100);       
+        
         Turret2Rect = new Rect(x + 100, y, 100, 100);
+
+        Turret3Rect = new Rect(x + 200, y ,100, 100);
+
+        TurretBoarder = new Rect(x - 5, y - 10, 400,110);
+
+
+        const float pauseWidth = 350f;
+        const float pauseHeight = 500f;
+
+        var pauseX = (Screen.width/2) - (pauseWidth/2);
+        var pauseY = (Screen.height/2) - (pauseHeight/2);
+
+        _pauseMenuRect = new Rect(pauseX,pauseY,pauseWidth,pauseHeight);
 
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+
+        //T Will Toggle Turret Menu
+	    if (Input.GetKeyDown(KeyCode.T))
+	    {
+	        TurretMenu = !TurretMenu;
+	    }
+
+
+	    if (!GamePaused)
+	    {
+	        GamePaused = Input.GetKeyDown(KeyCode.Escape);
+	    }
+	    if (GamePaused)
+	    {
+	        Pausegame();
+	    }
+
 	}
 
 
 
     void OnGUI()
     {
-       
-        UnityEngine.GUI.DrawTexture(Turret1Rect, turret1texture); //ScaleMode.ScaleToFit,true, 10.0f);
-
-        UnityEngine.GUI.DrawTexture(Turret2Rect,turret2Texture);
 
 
-        if ( Input.GetMouseButton(0))
+        if (GamePaused)
         {
-            Debug.Log(Turret1Rect.Contains(Input.mousePosition));
-            if (Turret1Rect.Contains(Input.mousePosition))
-            {
-                Debug.Log("Turret 1 clicked");
-            }
-            if (Turret2Rect.Contains(Input.mousePosition))
-            {
-                Debug.Log("Turret 2 clicked");
-            }
-            Debug.Log(Input.mousePosition.ToString());
+            _pauseMenuRect = UnityEngine.GUI.Window(2, _pauseMenuRect, PauseMenu, "");
         }
 
+        //Active Turret
+        UnityEngine.GUI.DrawTexture(ActiveTurret,ActiveTurretTexture2D);
+
+        if (TurretMenu)
+        {
+            //Turret Menu stuff
+            UnityEngine.GUI.DrawTexture(Turret1Rect, turret1texture2D);
+
+            UnityEngine.GUI.DrawTexture(Turret2Rect, turret2Texture2D);
+
+            UnityEngine.GUI.DrawTexture(Turret3Rect, Turret3Texture2D);
+
+            UnityEngine.GUI.Box(TurretBoarder, ""); 
+
+            SelectTurret();
+        }
+       
+
+    
+
         //TurretSelector = UnityEngine.GUI.Window(0, TurretSelector, PauseMenu, "");
+    }
+
+
+    public void SelectTurret()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Stupid Unity Making me do stupid offset
+            var clickedOnScreen = Input.mousePosition;
+            clickedOnScreen.y = Screen.height - clickedOnScreen.y;
+
+            if (Turret1Rect.Contains(clickedOnScreen))
+            {
+                ActiveTurretTexture2D = turret1texture2D;
+            }
+            if (Turret2Rect.Contains(clickedOnScreen))
+            {
+                ActiveTurretTexture2D = turret2Texture2D;
+            }
+            if (Turret3Rect.Contains(clickedOnScreen))
+            {
+                ActiveTurretTexture2D = Turret3Texture2D;
+            }
+        }
+    }
+
+
+    void Pausegame()
+    {
+        Time.timeScale = 0;
+    }
+
+    void UnPause()
+    {
+        Time.timeScale = 1;
+        GamePaused = false;
     }
 
 
@@ -85,11 +159,11 @@ public class GUI : MonoBehaviour {
 
         
 
-       // GUILayout.Label("Paused");
+        GUILayout.Label("Paused");
 
-/*        if (GUILayout.Button("Unpause"))
+        if (GUILayout.Button("Unpause"))
         {
-          //  UnPause();
+            UnPause();
         }
 
         if (GUILayout.Button("Restart Level"))
@@ -105,9 +179,9 @@ public class GUI : MonoBehaviour {
          //   UnPause();
             //Go to the Main Menu
          //   MainMenu();
-        }*/
+        }
 
-        GUILayout.Space(8);
+     //   GUILayout.Space(8);
         GUILayout.EndVertical();
         //GUI.DragWindow(new Rect(0, 0, 10000, 10000));
 
