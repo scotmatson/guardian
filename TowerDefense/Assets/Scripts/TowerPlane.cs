dd453ed5fb20073280994beb9a8f0b6cefaf50e8
frizzlefry22 <frizzlefry22@gmail.com>
2014-12-06 17:49:07 -0800

    Adding Tower stuff

---
+++ TowerDefense/Assets/Scripts/TowerPlane.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerPlane : MonoBehaviour
+{
+
+
+    public bool IsAvailable;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnMouseClick()
+    {
+        
+    }
+}
