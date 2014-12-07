2e9f29ff2316126dc3c6f89f6179db20c5c9211f
Scot Matson <sjpmatson@gmail.com>
2014-12-06 20:49:26 -0800

    Enemy now travels along the path, rotates, and is destroyed if it reaches the player base

--- TowerDefense/Assets/Scripts/EnemyPath.cs
+++ TowerDefense/Assets/Scripts/EnemyPath.cs
@@ -7,14 +7,16 @@ public class EnemyPath : MonoBehaviour {
     private GameObject wpContainer;
     private Transform[] waypoints;
     private Vector3 currentPosition;
-    public float speed;
+    public float travelSpeed;
+    public float rotationalSpeed;
     public int wpNum;
 
 	// Use this for initialization
 	void Start () {
-        //The speed of the game object
-        speed = 5.0f;
-
+        //The travel speed of the game object
+        travelSpeed = 5.0f;
+        //The rotational speed of game object
+        rotationalSpeed = 10.0f;
         //Locate the parent container which holds all of the waypoints
         wpContainer = GameObject.Find("Waypoints");
         //Collects the (x, y, z) position of all waypoints into an Array
@@ -27,17 +29,16 @@ public class EnemyPath : MonoBehaviour {
 	void Update () {
         var currentPosition = transform.position;
         var nextPosition = waypoints[wpNum].position;
+        var rotation = Quaternion.LookRotation(nextPosition - currentPosition);
         if (currentPosition != nextPosition) {
-            transform.position = Vector3.MoveTowards(currentPosition, nextPosition, speed * Time.deltaTime);
-            //transform.position = new Vector3(nextPosition.x, nextPosition.y, nextPosition.z);
-            //transform.Translate((waypoints[wpNum].position - currentPosition) * Time.deltaTime * speed);
-            //if (currentPosition == nextPosition)
-                //++wpNum;
+            //Faces enemy towards next waypoint
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationalSpeed);
+            //Moves enemy towards next waypoint
+            transform.position = Vector3.MoveTowards(currentPosition, nextPosition, travelSpeed * Time.deltaTime);
         }
         if (currentPosition == nextPosition && wpNum < waypoints.Length - 1) {
             ++wpNum;
         }
-        
 	}
 }
 
