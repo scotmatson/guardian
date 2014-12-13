8e375f4f363a55276dee1763c1d2e4acb8d908f1
Scot Matson <sjpmatson@gmail.com>
2014-12-13 15:06:02 -0800

    New asset, new level 1, blah blah blah. Needs tower tiles

--- TowerDefense/Assets/Scripts/EnemyPath.cs
+++ TowerDefense/Assets/Scripts/EnemyPath.cs
@@ -7,22 +7,19 @@ public class EnemyPath : MonoBehaviour {
     private GameObject wpContainer;
     private Transform[] waypoints;
     private Vector3 currentPosition;
-    public float travelSpeed;
-    public float rotationalSpeed;
-    public int wpNum;
+    //The travel speed of the game object
+    public float travelSpeed = 5.0f;
+    //The rotational speed of game object
+    public float rotationalSpeed = 10.0f;
+    //The starting index for waypoints
+    public int wpNum = 1;
 
 	// Use this for initialization
 	void Start () {
-        //The travel speed of the game object
-        travelSpeed = 5.0f;
-        //The rotational speed of game object
-        rotationalSpeed = 10.0f;
         //Locate the parent container which holds all of the waypoints
         wpContainer = GameObject.Find("Waypoints");
         //Collects the (x, y, z) position of all waypoints into an Array
         waypoints = wpContainer.GetComponentsInChildren<Transform>();
-        //The starting index for waypoints
-        wpNum = 2;
     }
 	
 	// Update is called once per frame
