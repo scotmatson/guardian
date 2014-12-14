1e3da4ae15baeb135bc68bc46aebbbde444d783e
frizzlefry22 <frizzlefry22@gmail.com>
2014-12-14 00:35:59 -0800

    NEw Turrets and stuff

---
+++ TowerDefense/Assets/Scripts/EnemyState.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyState : MonoBehaviour
+{
+
+    public int  Health;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	    Health = (Health == 0) ? 5 : Health;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void DoDamage(int damage)
+    {
+        Health -=  damage;
+
+        if (Health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ TowerDefense/Assets/Scripts/Missile.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Missile : MonoBehaviour {
+
+    public GameObject CurrentTarget;
+    public float Speed;
+
+    public bool HadTarget;
+
+    public bool InPursuit;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Speed = (Speed == 0) ? Speed = 20 : Speed;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Had true just incase the Update is rabn before CurrentTarget set
+        if (HadTarget && CurrentTarget == null)
+        {
+            Destroy(gameObject);
+        }
+
+        if (CurrentTarget != null)
+        {
+            PursuitPlayer();
+            HadTarget = true;
+        }
+        
+    }
+
+    void PursuitPlayer()
+    {
+
+        var newRotation = Quaternion.LookRotation(CurrentTarget.transform.position - transform.position).eulerAngles;
+        newRotation.z = 0;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(newRotation), 1);
+
+        //If we want to move with updating pos
+        transform.position += transform.forward * Speed * Time.deltaTime;
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+         if (other.gameObject.tag == "Enemy")
+         {
+             //Calls the method DoDamage on all MonoBehaviours(Scripts) attached to this GameObject
+             other.gameObject.SendMessage("DoDamage",3);
+             
+             //Destroy the Missile
+             Destroy(gameObject);
+         }
+    }
+
+}
