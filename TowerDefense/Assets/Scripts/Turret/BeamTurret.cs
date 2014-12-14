a2f1ad6defbc80a1dedc7b48b177ca97fdbc8548
frizzlefry22 <frizzlefry22@gmail.com>
2014-12-13 17:11:57 -0800

    temp stuff

---
+++ TowerDefense/Assets/Scripts/Turret/BeamTurret.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeamTurret : MonoBehaviour
+{
+
+
+    public bool BeamExists;
+
+    public GameObject beam;
+
+	// Use this for initialization
+	void Start () {
+	
+
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+
+
+
+	}
+
+
+    void Shoot()
+    {
+        Instantiate(beam, transform.position, transform.rotation);
+        BeamExists = true;
+    }
+
+    void OnMouseDown()
+    {
+        Shoot();
+    }
+
+
+}
