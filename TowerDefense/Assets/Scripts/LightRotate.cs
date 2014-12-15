63cf9fa50dbdb49259786b9035d83cd85898539e
Scot Matson <sjpmatson@gmail.com>
2014-12-14 17:04:02 -0800

    Level 3 pretty much finished. New assets.

---
+++ TowerDefense/Assets/Scripts/LightRotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightRotate : MonoBehaviour {
+
+    public bool rotateRight = true;
+	public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+		rotateSpeed = 200.0F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (rotateRight)    //Rotate right
+            transform.Rotate(Vector3.up, Time.deltaTime * rotateSpeed);
+        else                //Rotate left
+            transform.Rotate(Vector3.down, Time.deltaTime * rotateSpeed);
+	}
+}
