938e8da26cbd662acc7b8ab26e908aacb21c13e4
Scot Matson <sjpmatson@gmail.com>
2014-12-16 11:43:45 -0800

    Jets on level 3 now have a cool flyby sound effect

---
+++ TowerDefense/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioPlayer : MonoBehaviour {
+
+    //Takes an AudioSource component (Which needs to have an audio clip assigned to it)
+    //and a game object which will trigger the audio clip. You should be able to easily place
+    //this anywhere in the game to add sound triggers.
+    public AudioSource audioSource;
+    public GameObject MyGameObject;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == MyGameObject.tag)
+        {
+            AudioSource.PlayClipAtPoint(audioSource.clip, MyGameObject.transform.position);
+        }
+    }
+}
